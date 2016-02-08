cf51113b49f12e1d5a811a6503f9f35f107d59cf
Roman Novosad <r.novosad@outlook.com>
2016-02-08 10:57:46 +0200

    Continue with learning

---
+++ TermProj/term/term/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  term
+//
+//  Created by Roman Novosad on 2/7/16.
+//  Copyright © 2016 Roman Novosad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
